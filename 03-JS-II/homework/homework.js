61b638983a211b2b8c12ba9d4fadfefbdcd52f34
LorenaJurado <lorejurado89@gmail.com>
2022-08-09 18:54:30 -0500

    09 ago

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -128,15 +128,10 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
-if (numero%3 === 0){
-  return "fizz"
-}else if (numero%5===0){
-  return "buzz"
-}else if (numero%15===0){
-  return "fizzbuzz"
-}else{
-return numero
-}
+  if(numero % 15 === 0) return 'fizzbuzz';
+  if(numero % 3 === 0) return 'fizz';
+  if(numero % 5 === 0) return 'buzz';
+  return numero;
 }
 
 function operadoresLogicos(num1, num2, num3) {
@@ -147,16 +142,20 @@ function operadoresLogicos(num1, num2, num3) {
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
   
-  if (num1>0 && num1>num2 && num1>num3 ){
-    return "Número 1 es mayor y positivo"
-  }else if (num1<0 ||num2<0 ||num3<0){
-  return "Hay negativos"
-  }else if (num3>num1 && num3>num2){
-    return num3+1
-  }else if (num1 ===0 || num2 ===0 || num3===0){
-  return "Error"
-  }else{
-  return false
+  if(num1 < 0 || num2 < 0 || num3 < 0) {
+    return "Hay negativos";
+  }
+  else if(num1 === 0 || num2 === 0 || num3 === 0) {
+    return "Error";
+  }
+  else if(num1 > 0 && num1 > num2 && num1 > num3) {
+    return "Número 1 es mayor y positivo";
+  }
+  else if(num3 > num1 && num3 > num2) {
+    return num3 + 1;
+  }
+  else {
+    return false;
   }
 }
 
@@ -167,17 +166,21 @@ function esPrimo(numero) {
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
 
-if (numero/numero >0 && numero%1===0){
-  return true
-}else
-return "falso"
+  if( numero < 2) return false;
+  if(numero === 2) return true;
+  for(var i = 2; i < numero; i++) {
+    if(numero % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
-if (valor ===Boolean){
+if (valor ===true){
   return "Soy verdadero"
 }else
 return "Soy falso"
